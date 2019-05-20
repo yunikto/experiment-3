65e30287b06536e22ce25bd1d286e907ee294d4e
yunikto <47809989+yunikto@users.noreply.github.com>
2019-05-20 22:25:56 +0800

    Add files via upload

---
+++ exp 3 no 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int x, y[10]; 
+	double s, l;
+	double sum=0;
+	cout<< "Enter 10 numbers"<< endl;
+	for(x=0;x<10;x++)
+	{
+		cin>>y[x];
+	}
+	for(x=0;x<10;x++)
+	{
+		sum=sum+y[x];
+	}
+	s= y[0];
+	l= y[0];
+	for (int i=0;i<10;i++)
+	{	if(y[i]>l)
+		{
+			l=y[i];
+		}
+		if(y[i]<s)
+		{
+			s=y[i];
+		}
+	}
+	cout<< "Largest integer: "<< l<< endl;
+	cout << "Smallest integer: "<< s<< endl;
+	cout << "The total is: "<< sum<< endl;
+	cout << "The average is: " << sum/10<< endl;
+	getch();
+	return 0;
+}
